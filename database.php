2f42e605039b79a4d1ae6117979504e53981f700
Juan-Hilario <juanhilario5645@gmail.com>
2025-04-29 12:04:53 -0400

    Added php connection to database
    If you try to use this right now make sure to change the dbName, userName, and password in database.php

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+    $dbName = 'hilarij1_LillyOfTheValley';
+    $userName= 'hilarij1_user';
+    $password= 'a%mZuF])Oc!2';
+    
+    @ $db = new mysqli('localhost', $userName, $password, $dbName);
+?>
